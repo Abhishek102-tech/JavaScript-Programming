ed18177d3d296c880c0349320dd7883cc7f022be
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2024-10-21 20:17:59 +0530

    String Methods

---
+++ Lecture 3/str2.js
@@ -0,0 +1,8 @@
+let str="Abhishek Kumar";
+let upstr=str.toUpperCase();
+let lowstr=str.toLowerCase();
+let tstr=str.trim();
+console.log(str);
+console.log(upstr);
+console.log(lowstr);
+console.log(tstr);
