8c6b2dcc70fbedff9b82a6a5d872731364561489
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2025-03-25 22:27:48 +0530

    made some changes

---
+++ Rock paper scissors game/script.js
@@ -0,0 +1,12 @@
+let rock=document.querySelector(".rock")
+let paper=document.querySelector(".paper")
+let scissor=document.querySelector(".scissor")
+
+let userScore=0;
+let computerScore=0;
+
+let result=document.querySelector(".result")
+
+rock.addEventListener("click",()=>{
+    
+})
