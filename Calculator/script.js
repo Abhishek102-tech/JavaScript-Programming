200f28c72173729b544262a8cab39bf1d1364150
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2025-03-29 19:18:46 +0530

    added js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,6 +1,34 @@
-let screen=document.querySelector('.screen');
-let buttons=document.querySelectorAll('.btn');
+let screen = document.querySelector('.screen');
+let buttons = document.querySelectorAll('.btn');
 
-buttons.addEventListener('click', (e) => {
-    screen.value += e.target.innerText;
-})
+buttons.forEach(b => {
+    b.addEventListener('click', (e) => {
+        console.log(e)
+        if(e.target.id=="negative"){
+            screen.innerText = -1 * parseFloat(screen.innerText);
+            return;
+        }
+        if(e.target.id=="clear"){
+            screen.innerText = screen.innerText.slice(0,screen.innerText.length-1);
+            if(screen.innerText.length==0 || screen.innerText=="-"){
+                screen.innerText = 0;
+            }
+            return;
+        }
+        if(e.target.id=="restart"){
+            screen.innerText = 0;
+            return;
+        }
+        if(e.target.id=="equal"){
+            let k=screen.innerText.replaceAll("X","*");
+            screen.innerText = eval(k);
+            return;
+        }
+        if(screen.innerText=="0"){
+            screen.innerText = e.target.innerText;
+            return;
+        }
+        
+        screen.innerText += e.target.innerText;
+    }
+    )})
